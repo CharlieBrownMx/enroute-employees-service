0b39ec984a27c688e3e70063ba65b4326ddd4ab3
Esteban Cruz Torres <carlos.cruz19@outlook.fr>
2021-10-24 21:24:15 -0500

    Package with utilities that might be useful across files.

---
+++ app/src/utilities/serviceTools.ts
@@ -0,0 +1,27 @@
+import Joi from 'joi'
+
+class ServiceTools {
+  static validateEmployee = (employee: Object) => {
+    return Joi.object({
+      emp_no: Joi.number()
+        .min(1)
+        .required()
+        .error(new Error("The parameter 'id' must be a number")),
+      birth_date: Joi.date().required().error(new Error("Date format should be: 'YYYY-MM-DD'")),
+      first_name: Joi.string().min(1).max(14).required(),
+      last_name: Joi.string().min(1).max(16).required(),
+      gender: Joi.string().valid('M', 'F'),
+      hire_date: Joi.date().required().error(new Error("Date format should be: 'YYYY-MM-DD'")),
+    }).validate(employee)
+  }
+
+  static validateEmployeeId = (id: string) => {
+    return Joi.number()
+      .min(1)
+      .required()
+      .error(new Error("The parameter 'id' must be a number"))
+      .validate(id)
+  }
+}
+
+export default ServiceTools

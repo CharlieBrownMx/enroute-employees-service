15bf592aac1edfc8751d752bb4fcc573d21483a8
Esteban Cruz Torres <carlos.cruz19@outlook.fr>
2021-10-24 21:21:05 -0500

    Error handler middleware in case there's any unhandled exception.

---
+++ app/src/middleware/error.middleware.ts
@@ -0,0 +1,20 @@
+import { NextFunction, Request, Response } from 'express'
+import { HttpRequestException } from '../exceptions/HttpRequestExceptions'
+
+const errorMiddleware = (error: HttpRequestException, request: Request, response: Response, next: NextFunction) => {
+  const statusCode = error.statusCode || 500
+  const message = error.message || '/! Oh no! Something went wrong /!\\'
+  const type = error.type || 'HttpUnkownException'
+  const moreInfo = error.moreInfo || 'No more info available'
+  const success = error.success || false
+
+  response.status(statusCode).send({
+    statusCode,
+    type,
+    message,
+    moreInfo,
+    success,
+  })
+}
+
+export default errorMiddleware
